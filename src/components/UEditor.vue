bf8474b00b6242b5773649c5c19763bc966b5af4
yefan <yefan@keduxinxi.com>
2017-03-23 10:29:03 +0800

    编辑器初始化setContent

--- src/components/UEditor.vue
+++ src/components/UEditor.vue
@@ -105,6 +105,7 @@ export default {
           // 绑定事件，当 UEditor 初始化完成后，将编辑器实例通过自定义的 ready 事件交出去
           this.instance.addListener('ready', () => {
             this.$emit('ready', this.instance);
+            this.instance.setContent(this.value);
           });
           // 监听编辑器内容变化，将编辑器的值set至model
           this.instance.addListener('contentChange', () => {
