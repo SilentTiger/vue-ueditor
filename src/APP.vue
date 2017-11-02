82ae05c0c28da395424911e2e315fd9e0f5b498f
wyj <ismrwang@gmail.com>
2017-11-02 17:16:30 +0800

    增加了类似v-model的解决方案

--- src/APP.vue
+++ src/APP.vue
@@ -1,21 +1,37 @@
 <template>
-  <div id="app">
-    <VueUEditor @ready="editorReady" style="width: 800px"></VueUEditor>
+  <div id='app'>
+    <VueUEditor @ready="editorReady" :defaultMsg="defaultMsg" style="width: 800px" ref="ue"></VueUEditor>
+    <button @click="handleSubmit">获取内容</button>
   </div>
 </template>
 
 <script>
-  import VueUEditor from './components/UEditor';
-  export default {
-    name: 'app',
-    components: { VueUEditor },
-    methods: {
-      editorReady (editorInstance) {
-        editorInstance.setContent('Hello world!<br>你可以在这里初始化编辑器的初始内容。');
-        editorInstance.addListener('contentChange', () => {
-          console.log('编辑器内容发生了变化：', editorInstance.getContent());
-        });
-      }
+import VueUEditor from './components/UEditor';
+export default {
+  name: 'app',
+  components: { VueUEditor },
+  data () {
+    return {
+      defaultMsg: 'Demo Text...'
+    };
+  },
+  methods: {
+    editorReady (editorInstance) {
+      editorInstance.setContent('Hello world!<br>你可以在这里初始化编辑器的初始内容。');
+      editorInstance.addListener('contentChange', () => {
+        console.log('编辑器内容发生了变化：', editorInstance.getContent());
+      });
+    },
+    // 表单提交前获取 UEditor 内容
+    handleSubmit () {
+      console.log(this.$refs.ue.getUEContent());
     }
-  };
+  },
+  mounted () {
+    // mock response as server
+    setTimeout(() => {
+      this.defaultMsg = 'You have received a message.';
+    }, 2000);
+  }
+};
 </script>
